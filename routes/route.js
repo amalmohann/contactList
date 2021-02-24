d8ea09551d7687b3fe64eae7ab72d6060d5953d6
amalmohann <amal.mohan@storilabs.com>
2021-02-24 22:00:32 +0530

    create read and delete routing

--- routes/route.js
+++ routes/route.js
@@ -4,8 +4,15 @@ const express = require('express')
 //importing express Router
 const router = express.Router()
 
+//for retrieving data
 router.get('/contacts',(req,res,next)=>{
     res.send('retrieving the contacts')
+    //logic to retrieve data
+})
+
+//for adding data
+router.get('contact/add',(req,res,next)=>{
+    //logic to add data(contacts)
 })
 
 module.exports = router
